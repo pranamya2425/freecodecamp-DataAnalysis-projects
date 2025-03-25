29ea0781799cb2478994ef83c107094346a278e2
D.M.S Pranamya <145209850+pranamya2425@users.noreply.github.com>
2025-03-25 23:06:31 +0530

    Update sea_level_predictor.py

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -11,7 +11,6 @@ def draw_plot():
     slope, intercept, r_value, p_value, std_err = linregress(df["Year"], df["CSIRO Adjusted Sea Level"])
     years = pd.Series(range(1880,2050))
     ax.plot(years, intercept + slope*years, 'r', label='first line of best fit')
-    # Create second line of best fit
     df2 = df.loc[df["Year"] >= 2000]
     slope2, intercept2, r_value2, p_value2, std_err2 = linregress(df2["Year"], df2["CSIRO Adjusted Sea Level"])
     years2 = pd.Series(range(2000,2050))
